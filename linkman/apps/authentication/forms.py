995e87986eb1337989e6751a03d16c174eb86505
jovbk <jovani.badinga7@gmail.com>
2025-12-03 23:42:04 -0500

    Fixed mypy and pyright warnings

--- linkman/apps/authentication/forms.py
+++ linkman/apps/authentication/forms.py
@@ -1,22 +1,33 @@
 """
 This module stores forms used for authentication
 """
+
+from typing import Any
+
 from django import forms
 
 
 class SignupForm(forms.Form):
     email = forms.EmailField(max_length=254, min_length=3, required=True)
-    password_one = forms.CharField(widget=forms.PasswordInput, required=True,max_length=265, min_length=8)
-    password_two = forms.CharField(widget=forms.PasswordInput, required=True,max_length=265, min_length=8)
+    password_one = forms.CharField(
+        widget=forms.PasswordInput, required=True, max_length=265, min_length=8
+    )
+    password_two = forms.CharField(
+        widget=forms.PasswordInput, required=True, max_length=265, min_length=8
+    )
 
-    def clean(self):
+    def clean(self) -> dict[str, Any]:
         cleaned_data = super(SignupForm, self).clean()
+        assert cleaned_data is not None  # to get rid of pyright warnings
         password_one = cleaned_data.get("password_one")
         password_two = cleaned_data.get("password_two")
         if password_one != password_two:
             raise forms.ValidationError("Passwords do not match!")
         return cleaned_data
 
+
 class LoginForm(forms.Form):
     email = forms.EmailField(max_length=254, min_length=3, required=True)
-    password = forms.CharField(widget=forms.PasswordInput, required=True,max_length=265, min_length=8)
+    password = forms.CharField(
+        widget=forms.PasswordInput, required=True, max_length=265, min_length=8
+    )
