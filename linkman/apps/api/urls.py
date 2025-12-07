8f18748aa3c7ca867e8ad2a44c1a07b11dc2a091
jovbk <jovani.badinga7@gmail.com>
2025-12-07 11:53:38 -0500

    Updated the `dashboard.html`
    1. Removed the sample links
    
    2. Created two new modals
    - Add group
    - Add link

--- linkman/apps/api/urls.py
+++ linkman/apps/api/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 from ..api import views
 
 urlpatterns = [
-    path("groups/", views.get_groups, name="get_groups"),
-    path("links/", views.get_links, name="get_links"),
+    path("groups/", views.handle_groups, name="groups"),
+    path("links/", views.handle_links, name="links"),
 ]
--- linkman/apps/api/views.py
+++ linkman/apps/api/views.py
@@ -3,11 +3,11 @@ from django.http.response import JsonResponse
 from ..main.models import Group, Link
 
 
-def get_groups(request) -> JsonResponse:
+def handle_groups(request) -> JsonResponse:
     groups = list(Group.objects.filter(user=request.user).values())
     return JsonResponse({"groups": groups})
 
 
-def get_links(request) -> JsonResponse:
+def handle_links(request) -> JsonResponse:
     links = list(Link.objects.filter(user=request.user).values())
     return JsonResponse({"links": links})
